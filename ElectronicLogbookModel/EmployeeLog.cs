b3b78b7584273b2620d0b0a71b97352f1134492d
Adrianne Claude Tubig <adriannetubig@gmail.com>
2018-06-06 14:04:52 +0800

    Refactored Employee Function, Model and Controller

--- ElectronicLogbookModel/EmployeeLog.cs
+++ ElectronicLogbookModel/EmployeeLog.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using BaseModel;
-using System.ComponentModel.DataAnnotations;
+﻿using BaseModel;
+using System;
 
 namespace ElectronicLogbookModel
 {
     public class EmployeeLog : Base
     {
         public bool SuccesLogin { get; set; }
+
         public DateTime LogDate { get; set; }
+
         public int EmployeeLogId { get; set; }
         public int EmployeeId { get; set; }
         public int LogTypeId { get; set; }
-        [Required, Display(Name = "Employee Number"), StringLength(100)]
+
         public string EmployeeNumber { get; set; }
         public string EmployeeImageBase64 { get; set; }
-        public string LogName { get; set; }
-        [Required, Display(Name = "Pin"), DataType(DataType.Password), StringLength(100)]
-        public string Pin { get; set; }
-        public LogType LogType { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string MiddleName { get; set; }
+        public string Pin { get; set; }
+
+        public LogType LogType { get; set; }
     }
 }
