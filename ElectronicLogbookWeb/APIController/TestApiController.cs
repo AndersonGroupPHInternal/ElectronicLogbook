c66aabe80ff62761df1c38bc13888c112634bd96
Adrianne Claude Tubig <adriannetubig@gmail.com>
2018-06-21 17:08:12 +0800

    https://trello.com/c/z9LPELo9/218-2018-04-11-create-web-api-to-retrieve-logbook-data   Enabled API Controllers (test controllers only)

---
+++ ElectronicLogbookWeb/APIController/TestApiController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace ElectronicLogbookWeb.APIController
+{
+    public class TestApiController : BaseApiController
+    {
+        [HttpPost]
+        public IHttpActionResult LoggedOut()
+        {
+            return Ok();
+        }
+    }
+}
