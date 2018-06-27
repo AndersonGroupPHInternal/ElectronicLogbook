9d4473d88189bca910c33e4bbf6cef509ea04456
Adrianne Claude Tubig <adriannetubig@gmail.com>
2018-06-27 16:55:31 +0800

    https://trello.com/c/4HxNzZQF/220-2018-04-11-create-an-exe-file-to-trigger-the-sending-of-email   Created consumer project

---
+++ ElectronicLogbookConsumer/EmployeeLogApi.cs
@@ -0,0 +1,20 @@
+﻿using ElectronicLogbookModel;
+using ElectronicLogbookModel.Filter;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ElectronicLogbookConsumer
+{
+    public class EmployeeLogApi : ElectronicLogbookConsumerBase, IEmployeeLogApi
+    {
+        public EmployeeLogApi()
+        {
+        }
+
+        public async Task<List<EmployeeLog>> Create(EmployeeLogFilter employeeLogFilter)
+        {
+            DestinationUrl = "/api/EmployeeLogApi/Read";
+            return await PostWithAuthentication<List<EmployeeLog>, EmployeeLogFilter>(employeeLogFilter);
+        }
+    }
+}
