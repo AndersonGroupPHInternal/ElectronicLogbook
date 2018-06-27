bad4414a1a5e5c30338c79c074259e065d68458f
Adrianne Claude Tubig <adriannetubig@gmail.com>
2018-06-27 16:00:17 +0800

    https://trello.com/c/z9LPELo9/218-2018-04-11-create-web-api-to-retrieve-logbook-data   Created api on searching work logs   Created Postman files

---
+++ ElectronicLogbookWeb/APIController/EmployeeLogApiController.cs
@@ -0,0 +1,27 @@
+﻿using AndersonCRMFunction;
+using ElectronicLogbookFunction;
+using ElectronicLogbookModel.Filter;
+using System.Web.Http;
+
+namespace ElectronicLogbookWeb.APIController
+{
+    public class EmployeeLogApiController : BaseApiController
+    {
+        private IFEmployeeLog _iFEmployeeLog;
+        private IFEmployee _iFEmployee;
+
+        public EmployeeLogApiController()
+        {
+            _iFEmployeeLog = new FEmployeeLog();
+            _iFEmployee = new FEmployee();
+
+        }
+
+        [HttpPost]
+        public IHttpActionResult Read(EmployeeLogFilter employeeLogFilter)
+        {
+            var employeeLogs = _iFEmployeeLog.Read(employeeLogFilter);
+            return Ok(employeeLogs);
+        }
+    }
+}
